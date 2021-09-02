c7ccf0250418710536f4751adf74954e1be39d5a
ypolat99 <77054305+ypolat99@users.noreply.github.com>
2021-09-02 03:07:22 -0700

    Update Optimize.py
    Added necessa functions, bounds, initial guess, final output.
    Fixed lenght bug in initial guess. 
    May not be working haven't tested it all yet

--- Optimize.py
+++ Optimize.py
@@ -11,7 +11,23 @@ https://www.kaggle.com/jacksoncrow/stock-market-dataset
 Here etfs folder has data about many different ETFs and stokcs folder has data about many different stokcs. 
 """
 
+
+# Ask the User for the stock and ETF symbols
+#---------------------------------------------------
+def get_stock_list():
+  print("Please enter the stock/ETF symbols you want in your portfolio (Ex: AMZN)")
+    while True:
+        print("Please Press q to exit")
+        stock=  input("Enter your stock")
+        if stock == "q":
+          break
+        stock_list.append(stock)
+        
+get_stock_list()      
+#stock_list = ["DIS", "KO", "PEP", "WMT", "IVV", "VUG"]
+
 # Get The Data
+#---------------
 data_list = []
 
 for stock in stock_list:
@@ -25,9 +41,61 @@ for stock in stock_list:
   data_list.append(data)
 
   
-#Create the Data Frame
+# Create the Data Frame
+#---------------------------
 stocks = pd.concat(data_list, axis=1)
 stocks.columns = [name for name in stock_list]
 stocks.head()
+
+# Get Logarithmic Returns
+log_ret = np.log(stocks/stocks.shift(1))
+log_ret.head()
+
+
+# Create The Necessary Functions
+#-----------------------------------
+
+def neg_sharpe(weights):
+    return  get_ret_vol_sr(weights)[2] * -1
+# Contraints
+def check_sum(weights):
+    '''
+    Returns 0 if sum of weights is 1.0
+    '''
+    return np.sum(weights) - 1
+def get_ret_vol_sr(weights):
+    """
+    Takes in weights, returns array or return,volatility, sharpe ratio
+    """
+    weights = np.array(weights)
+    ret = np.sum(log_ret.mean() * weights) * 252
+    vol = np.sqrt(np.dot(weights.T, np.dot(log_ret.cov() * 252, weights)))
+    sr = ret/vol
+    return np.array([ret,vol,sr])
+
+
+
+# Set up the bounds & Optimize
+#-----------------------------------
+
+# 0-1 bounds for each weight needs to be in the form of a tuple. 
+bounds = [(0,1) for i in range(len(stock_list))]
+bounds = tuple(bounds)
+
+# Initial Guess (equal distribution)
+eql = 1.0/len(stock_list)
+init_guess = [eql for i in range(len(stock_list))]
+cons = ({'type':'eq','fun': check_sum})
+
+# Sequential Least SQuares Programming (SLSQP).
+opt_results = minimize(neg_sharpe,init_guess,method='SLSQP',bounds=bounds,constraints=cons)
+
+print("Your Optimum Wegihts are:")
+for stock, weight in zip(stock_list, opt_results.x):
+    print(stock + ": " + str(weight))
+print("--------------------------------------")
+print("Here are the details of your optimum results:")
+print(opt_results)
+
 
 
